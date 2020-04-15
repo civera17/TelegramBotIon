c7f10f191a25188b3d4fe8248e03151508307336
civera <xakirionok@gmail.com>
2020-04-15 20:30:34 +0300

    - message that contains "mihai" triggers backmessage - added dice - "anime" or "Mihai" + "loh" deletes message

--- src/main/java/IonLohBot.java
+++ src/main/java/IonLohBot.java
@@ -1,29 +1,48 @@
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendDice;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
 
 public class IonLohBot extends TelegramLongPollingBot {
 
     public void onUpdateReceived(Update update) {
+        String updmess = update.getMessage().getText().toLowerCase();
+        updmess.replaceAll("\\s","");
+        SendMessage message = new SendMessage().setChatId(update.getMessage().getChatId());
+        SendDice sendDice = new SendDice().setChatId(update.getMessage().getChatId());
+        DeleteMessage deleteMessage = new DeleteMessage().setChatId(update.getMessage().getChatId())
+                .setMessageId(update.getMessage().getMessageId());
         if (update.hasMessage() && update.getMessage().hasText()) {
-            String updmess = update.getMessage().getText().toLowerCase();
-            SendMessage message = new SendMessage();
             if (updmess.contains("ion")) {
-                message.setChatId(update.getMessage().getChatId())
-                        .setText("Ion loh");
+                message.setText("Ion loh");
             }
             if (updmess.contains("mihai")) {
-                message.setChatId(update.getMessage().getChatId())
-                        .setText("btw ,am auzit ca mihai ghenii, imho.");
+                message.setText("btw ,am auzit ca mihai ghenii, imho.");
             }
+            try {
+                execute(message);
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+            if (updmess.contains("dice") || updmess.contains("ruletca") || updmess.contains("ruletka")
+                    || updmess.contains("getrandom") || updmess.contains("shara")) {
                 try {
-                    execute(message); // Call method to send the message
+                    execute(sendDice);
+                } catch (TelegramApiException e) {
+                    e.printStackTrace();
+                }
+            }
+            if (updmess.contains("mihai") && updmess.contains("loh") || updmess.contains("anime")){
+                try {
+                    execute(deleteMessage);
                 } catch (TelegramApiException e) {
                     e.printStackTrace();
                 }
             }
         }
+    }
 
     public String getBotUsername() {
         return "Bot Ion";
